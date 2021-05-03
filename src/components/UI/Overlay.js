6680ef8dc2aa02e198ed43b625903df2aed9e208
Raul Mercado <raulmercadox@gmail.com>
2021-05-03 01:21:51 -0500

    Se quitan clases no usadas

--- src/components/UI/Overlay.js
+++ src/components/UI/Overlay.js
@@ -2,7 +2,7 @@ import styles from './Overlay.module.css';
 
 const Overlay = (props) => {
     return (
-    <div id='overlay' className={styles.overlay}>
+    <div className={styles.overlay}>
         <div className={styles.title}>Mensaje del Sistema</div>
         <div className={styles.message}>{props.message}</div>
         <div className={styles.buttons}>
