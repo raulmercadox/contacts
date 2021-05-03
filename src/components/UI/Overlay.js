3b924619575c42dabdb135eba0d9a22661c0638e
Raul Mercado <raulmercadox@gmail.com>
2021-05-03 00:06:16 -0500

    Se estiliza el formulario modal

--- src/components/UI/Overlay.js
+++ src/components/UI/Overlay.js
@@ -3,7 +3,11 @@ import styles from './Overlay.module.css';
 const Overlay = (props) => {
     return (
     <div className={styles.overlay}>
-        {props.message}
+        <div className={styles.title}>Mensaje del Sistema</div>
+        <div className={styles.message}>{props.message}</div>
+        <div className={styles.buttons}>
+            <button className={styles.button}>Cerrar</button>
+        </div>
     </div>
     )
 }
